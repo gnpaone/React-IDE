d2b848f3645c9de34d8ed58fc421eb3d5a609bad
gnpaone <gnpaone@users.noreply.github.com>
2022-11-13 15:10:12 +0000

    deploy: gnpaone/my-IDE@849e944231ef4f3166752bc3945b4ad7f68f8a86

---
+++ precache-manifest.4f73c209159a1807093df95ab9b02004.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1326b77123608607b803caa9f6a96719",
+    "url": "/React-IDE/index.html"
+  },
+  {
+    "revision": "f85d956d0228bea1bae9",
+    "url": "/React-IDE/static/css/main.6f7548fc.chunk.css"
+  },
+  {
+    "revision": "adadcad4c0391228bcc9",
+    "url": "/React-IDE/static/js/2.a34f2f73.chunk.js"
+  },
+  {
+    "revision": "6b1a3c20a3fdee10840b21f5d93ed497",
+    "url": "/React-IDE/static/js/2.a34f2f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9e6b874eea134c36f0c",
+    "url": "/React-IDE/static/js/3.a837f923.chunk.js"
+  },
+  {
+    "revision": "af27ba1628daecec15b79fdf3f8b016c",
+    "url": "/React-IDE/static/js/3.a837f923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "732a271e99f0ec8c47dc",
+    "url": "/React-IDE/static/js/4.9a2113a2.chunk.js"
+  },
+  {
+    "revision": "f7b599e3f88bf94d2af5d3f60873a311",
+    "url": "/React-IDE/static/js/4.9a2113a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457b256a34488c5ebec1",
+    "url": "/React-IDE/static/js/5.36bc322b.chunk.js"
+  },
+  {
+    "revision": "7a3ec979db4eb6fe6c35",
+    "url": "/React-IDE/static/js/6.674f7ac1.chunk.js"
+  },
+  {
+    "revision": "f85d956d0228bea1bae9",
+    "url": "/React-IDE/static/js/main.c22c478d.chunk.js"
+  },
+  {
+    "revision": "e20ebc6cbbb55ecb0356",
+    "url": "/React-IDE/static/js/runtime-main.a5d89af8.js"
+  }
+]);
